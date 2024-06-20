b0cf5b312a290d6824e513c6ed6690df466df8e7
AJ Wild <ajwild320@gmail.com>
2024-06-19 20:12:26 -0400

    Completed Hero Section - All grid items and animations successfully integrated and implemented

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
-    <div>Grid</div>
+    <section id='about'>
+        <BentoGrid>
+            {gridItems.map (({ id, title, description, className, img, imgClassName, titleClassName, spareImg }) => (
+                <BentoGridItem 
+                    id={id}
+                    key={id}
+                    title={title}
+                    description={description}
+                    className={className}
+                    img={img}
+                    imgClassName={imgClassName}
+                    titleClassName={titleClassName}
+                    spareImg={spareImg}
+                />
+            ))}
+        </BentoGrid>
+    </section>
   )
 }
 
