da46c432947ae35dcd696508801555fac2d11da3
AJ Wild <ajwild320@gmail.com>
2024-06-22 00:20:59 -0400

    Fixed bug with buttons not rendering properly on Iphones

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -10,7 +10,7 @@ const Approach = () => {
             <h1 className="heading">
                 My <span className="text-purple">Approach</span>
             </h1>
-            <div className="my-20 flex flex-col lg:flex-row items-center justify-center gap-4">
+            <div className="my-20 flex flex-col lg:flex-row items-center justify-center gap-4 z-0">
                 <Card
                     title="Planning & Strategy"
                     icon={<AceternityIcon order="Phase 1" />}
@@ -107,7 +107,7 @@ const AceternityIcon = ({ order }: { order : string }) => {
       <div>
           <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-5 py-2 text-white backdrop-blur-3xl text-2xl font-bold">
+              <span className="z-0 inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-5 py-2 text-white backdrop-blur-3xl text-2xl font-bold">
                   {order}
               </span>
           </button>
